5cc322e92db8d416dd52c11ee81f735030de8ec1
sarascript <sara.correas@protonmail.com>
2020-05-10 19:01:26 +0200

    Working

---
+++ CoreDataAuth/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  CoreDataAuth
+//
+//  Created by Sara Correas on 10/05/2020.
+//  Copyright © 2020 Sara Correas. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
