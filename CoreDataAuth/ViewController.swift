068bb95866946184bd3995a3ef8b73dd7ff7ac09
sarascript <sara.correas@protonmail.com>
2020-05-10 17:32:06 +0200

    Initial Commit

---
+++ CoreDataAuth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataAuth
+//
+//  Created by Sara Correas on 10/05/2020.
+//  Copyright © 2020 Sara Correas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
